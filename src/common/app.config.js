e7ac2cbf4402ceee696de2065b01e0cb494caa95
wukuy <wukuy@wukuydeMacBook-Air.local>
2022-11-01 11:59:18 +0800

    细节调整

--- src/common/app.config.js
+++ src/common/app.config.js
@@ -1,15 +1,14 @@
 // 配置请求主路径
+let url = ''
 
 // #ifndef MP-WEIXIN
-export const API_URL = import.meta.env.VITE_API_URL
+url = import.meta.env.VITE_API_URL
 // #endif
 
 // #ifdef MP-WEIXIN
 const envVersion = uni.getAccountInfoSync().miniProgram.envVersion
-let url = ''
 let envName = ''
 
-console.log(envVersion)
 if(envVersion === 'develop' || envVersion === 'trial') {
     url = import.meta.env.VITE_API_TEST_URL
 
@@ -25,5 +24,6 @@ if(envVersion === 'develop' || envVersion === 'trial') {
 
 console.log(`当前小程序版本为: ${envName}`)
 console.log(`接口url: ${url}`)
-export const API_URL = url
 // #endif
+
+export const API_URL = url
